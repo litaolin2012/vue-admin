43cdbd962b3e2bece4b2237fc16d4877dc8e399e
litaolin2012 <1306882949@qq.com>
2020-03-16 13:49:55 +0800

    第一次提交

---
+++ src/components/Layout/components/Sidebar/Logo.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="logo">
+    <i class="el-icon-discover"></i>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Logo'
+};
+</script>
+
+<style lang="scss" scoped>
+@import '@/styles/variables.scss';
+.logo {
+  height: $navHeight;
+  line-height: $navHeight;
+  font-size: 24px;
+  text-align: center;
+  color: #fff;
+}
+</style>
